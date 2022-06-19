e1dfedf967e9174b591c76dfa5da7c7c716749ba
yograjfire18 <yograjfire18@gmail.com>
2022-06-19 08:56:14 +0530

    Z01R: Don't build Snap
    Signed-off-by: yograjfire18 <yograjfire18@gmail.com>

--- device.mk
+++ device.mk
@@ -188,7 +188,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     libdng_sdk.vendor \
-    Snap \
     vendor.qti.hardware.camera.device@1.0
 
 # Common init scripts
