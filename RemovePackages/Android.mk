95679684a127a7abe99e71890fdbf936f60d23fd
yograjfire18 <yograjfire18@gmail.com>
2022-09-16 23:01:07 +0530

    Z01R: RemovePackages: Remove more packages to match system size

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,35 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Drive Maps MyVerizonServices SprintDM SprintHM YouTube YouTubeMusicPrebuilt PrebuiltGmail GoogleFeedback Videos Ornament Photos Tycho GoogleFeedback WellbeingPrebuilt SafetyHubPrebuilt SoundAmplifierPrebuilt CameraRoll
+LOCAL_OVERRIDES_PACKAGES := arcore \
+CalculatorGooglePrebuilt \
+CalendarGooglePrebuilt \
+CameraRoll \
+DevicePolicyPrebuilt \
+Drive \
+Gallery2 \
+GoogleCamera \
+GoogleFeedback \
+Maps \
+MyVerizonServices \
+Ornament \
+Photos \
+PrebuiltGmail \
+SafetyHubPrebuilt \
+SoundAmplifierPrebuilt \
+SprintDM \
+SprintHM \
+TrichromeLibrary \
+TrichromeLibrary-Stub \
+TurboPrebuilt \
+Tycho \
+Videos \
+WellbeingPrebuilt \
+YouTube \
+YouTubeMusicPrebuilt \
+PixelWallpapers2021 \
+PixelLiveWallpaperPrebuilt \
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
