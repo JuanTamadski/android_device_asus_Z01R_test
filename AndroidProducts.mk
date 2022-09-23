18bda204e9d885258ff8b51615fe13a0d2255648
yograjfire18 <yograjfire18@gmail.com>
2022-09-23 09:31:08 +0530

    Z01R: xdroid minimalising

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Z01R.mk
+    $(LOCAL_DIR)/xdroid_Z01R.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Z01R-user \
-    lineage_Z01R-userdebug \
-    lineage_Z01R-eng
+    xdroid_Z01R-user \
+    xdroid_Z01R-userdebug \
+    xdroid_Z01R-eng
